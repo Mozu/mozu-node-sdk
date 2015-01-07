eb6ab1904ad1961935777bb0aac1aeb6551735c9
james <zetlen@gmail.com>
2015-01-07 12:51:54 -0600

    platform test

---
+++ test/platform.js
@@ -0,0 +1,23 @@
+var setup = require('./utils/config');
+var when = require('when');
+
+describe('Platform service', function() {
+
+  before(setup);
+  
+  this.timeout(20000);
+
+  it('returns a Tenant from GetTenant', function(done) {
+
+    var tenant = setup.client.platform().tenant().getTenant({
+      tenantId: setup.client.getTenant()
+    });
+
+    when.join(
+      tenant.should.eventually.have.property('domain'),
+      tenant.should.eventually.have.property('id',2628),
+      tenant.should.eventually.have.property('sites').should.eventually.have.property('length')
+    ).should.notify(done);
+
+  });
+});
