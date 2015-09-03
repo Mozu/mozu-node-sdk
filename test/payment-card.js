963b2ed4b409c4fe036214329dfd1eb89939d655
james <zetlen@gmail.com>
2015-09-03 10:09:18 -0500

    adding mock for payment card request

--- test/payment-card.js
+++ test/payment-card.js
@@ -2,25 +2,54 @@ var setupChai = require('./utils/setup-assertion-library');
 var proxyConf = {
   plugins: [require('../plugins/fiddler-proxy')]
 };
+var express = require('express');
+var bodyParser = require('body-parser');
+
 
 describe('Payment Card Service', function() {
 
-  before(setupChai);
-  
-  this.timeout(20000);
-  var opts = {
-    cvv: "123",
-    cardType: "visa",
-    expireMonth: "11",
-    expireYear: "2019",
-    cardHolderName: "Demo User",
-    cardNumber: "4111111111111111",
-    cardIssueMonth: "01",
-    cardIssueYear: "14"
-  };
+  var opts;
+  var client;
+  var mock;
 
-  it('returns CardId from Commerce.Payments.publicCard.Cards', function(done) {
-    return require('../clients/commerce/payments/publicCard')(proxyConf).create(opts)
+  before(function() {
+    setupChai();
+    this.timeout(20000);
+    opts = {
+      cvv: "123",
+      cardType: "visa",
+      expireMonth: "11",
+      expireYear: "2019",
+      cardHolderName: "Demo User",
+      cardNumber: "4111111111111111",
+      cardIssueMonth: "01",
+      cardIssueYear: "14"
+    };
+
+    client = require('../clients/commerce/payments/publicCard')(proxyConf);
+    client.context.basePciUrl = "http://localhost:8009/"
+
+  });
+
+
+  
+
+  it('sends card data to Commerce.Payments.publicCard.Cards via basePCIUrl', function(done) {
+
+    mock = express();
+
+    mock.use(bodyParser.json());
+
+    mock.post('/payments/commerce/payments/cards/', function(req, res) {
+      req.body.should.have.property('cardNumber').that.equals('4111111111111111');
+      res.json({
+        id: '123456'
+      });
+    });
+
+    mock.listen(8009);
+
+    return client.create(opts)
       .should.eventually.have.property('id')
       .notify(done);
   });
