f86aee6559db74070f0a69148d74c3043b1bb1f2
james <zetlen@gmail.com>
2015-09-11 16:48:07 -0500

    try catch in errorify

--- utils/errorify.js
+++ utils/errorify.js
@@ -2,33 +2,37 @@ var extend = require('./tiny-extend');
 var util = require('util');
 module.exports = function errorify(res, additions) {
   "use strict";
-  if (typeof res === "string") {
-    return new Error(res);
-  }
-  
-  var err;
-  var message = res.message || res.body && res.body.message;
-  var stringBody = typeof res.body === "string" ? res.body : (Buffer.isBuffer(res.body) ? res.body.toString() : null);
-  var details = typeof res.body === "object" ? res.body : (typeof res === "object" ? res : {});
-
-  if (!message && stringBody) {
-    try {
-      details = JSON.parse(stringBody);
-      message = details.message || stringBody;
-    } catch(e) {
-      message = stringBody;
+  try {
+    if (typeof res === "string") {
+      return new Error(res);
     }
+    
+    var err;
+    var message = res.message || res.body && res.body.message;
+    var stringBody = typeof res.body === "string" ? res.body : (Buffer.isBuffer(res.body) ? res.body.toString() : null);
+    var details = typeof res.body === "object" ? res.body : (typeof res === "object" ? res : {});
+
+    if (!message && stringBody) {
+      try {
+        details = JSON.parse(stringBody);
+        message = details.message || stringBody;
+      } catch(e) {
+        message = stringBody;
+      }
+    }
+
+    if (additions) {
+      extend(details, additions);
+    }
+
+    message = (message || "Unknown error!") + formatDetails(details);
+
+    err = new Error(message);
+    err.originalError = details;
+    return err;
+  } catch(e) {
+    return e;
   }
-
-  if (additions) {
-    extend(details, additions);
-  }
-
-  message = (message || "Unknown error!") + formatDetails(details);
-
-  err = new Error(message);
-  err.originalError = details;
-  return err;
 };
 
 function formatDetails(deets) {
