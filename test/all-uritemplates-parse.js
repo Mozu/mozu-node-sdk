ef34c15590589bf20bc5237904947bc219369ed9
zetlen <zetlen@gmail.com>
2015-12-07 16:36:37 -0600

    add uritemplate parse test

---
+++ test/all-uritemplates-parse.js
@@ -0,0 +1,30 @@
+'use strict';
+var fs = require('fs');
+var test = require('tape');
+var acorn = require('acorn');
+var acornWalk = require('acorn/dist/walk');
+var glob = require('glob');
+var uriTemplate = require('uri-template');
+
+test('all uri templates in autogenned code are parseable', function(assert) {
+  let allClients = glob.sync('./clients/**/*.js');
+  assert.plan(allClients.length);
+  allClients.forEach(function(f) {
+    var t = acorn.parse(fs.readFileSync(f, 'utf8'));
+    var badness = '';
+    acornWalk.simple(t, {
+      Property: function(n) {
+        if (n.key && n.key.name === 'url') {
+          try {
+            uriTemplate.parse(n.value.value);
+          } catch(e) {
+            badness += 
+              '\n\nuri template "' + n.value.value + '" does not parse in ' +
+                f + ', with error: ' + e;
+          }
+        }
+      }
+    });
+  assert.notOk(badness, badness || 'uri templates good in ' + f);
+  });
+});
