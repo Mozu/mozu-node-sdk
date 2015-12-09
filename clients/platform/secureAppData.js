e3d62bd0828e481a17e06ec4ed3788da7553f63c
Sanjay Mandadi <sanjay.mandadi@gmail.com>
2015-12-09 15:42:31 -0600

    Remove "*" from url templates

--- clients/platform/secureAppData.js
+++ clients/platform/secureAppData.js
@@ -14,18 +14,18 @@ var Client = require('../../client'), constants = Client.constants;
 module.exports = Client.sub({
 	getDBValue: Client.method({
 		method: constants.verbs.GET,
-		url: '{+tenantPod}api/platform/secureappdata/{appKeyId}/{*dbEntryQuery}?responseFields={responseFields}'
+		url: '{+tenantPod}api/platform/secureappdata/{appKeyId}/{dbEntryQuery}?responseFields={responseFields}'
 	}),
 	createDBValue: Client.method({
 		method: constants.verbs.POST,
-		url: '{+tenantPod}api/platform/secureappdata/{appKeyId}/{*dbEntryQuery}'
+		url: '{+tenantPod}api/platform/secureappdata/{appKeyId}/{dbEntryQuery}'
 	}),
 	updateDBValue: Client.method({
 		method: constants.verbs.PUT,
-		url: '{+tenantPod}api/platform/secureappdata/{appKeyId}/{*dbEntryQuery}'
+		url: '{+tenantPod}api/platform/secureappdata/{appKeyId}/{dbEntryQuery}'
 	}),
 	deleteDBValue: Client.method({
 		method: constants.verbs.DELETE,
-		url: '{+tenantPod}api/platform/secureappdata/{appKeyId}/{*dbEntryQuery}'
+		url: '{+tenantPod}api/platform/secureappdata/{appKeyId}/{dbEntryQuery}'
 	})
 });
--- clients/platform/siteData.js
+++ clients/platform/siteData.js
@@ -14,18 +14,18 @@ var Client = require('../../client'), constants = Client.constants;
 module.exports = Client.sub({
 	getDBValue: Client.method({
 		method: constants.verbs.GET,
-		url: '{+tenantPod}api/platform/sitedata/{*dbEntryQuery}?responseFields={responseFields}'
+		url: '{+tenantPod}api/platform/sitedata/{dbEntryQuery}?responseFields={responseFields}'
 	}),
 	createDBValue: Client.method({
 		method: constants.verbs.POST,
-		url: '{+tenantPod}api/platform/sitedata/{*dbEntryQuery}'
+		url: '{+tenantPod}api/platform/sitedata/{dbEntryQuery}'
 	}),
 	updateDBValue: Client.method({
 		method: constants.verbs.PUT,
-		url: '{+tenantPod}api/platform/sitedata/{*dbEntryQuery}'
+		url: '{+tenantPod}api/platform/sitedata/{dbEntryQuery}'
 	}),
 	deleteDBValue: Client.method({
 		method: constants.verbs.DELETE,
-		url: '{+tenantPod}api/platform/sitedata/{*dbEntryQuery}'
+		url: '{+tenantPod}api/platform/sitedata/{dbEntryQuery}'
 	})
 });
