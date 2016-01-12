c2fdb2c73390134f86a62636e66a8e92a7c9a8c8
allenmichael <allenmichaelgrobelny@gmail.com>
2016-01-12 15:45:48 -0600

    Added functionality to handle form data on Express request objects as handled by body-parser for Mozu POST requests sent by Subnavlinks and Configuration URL

--- security/is-request-valid.js
+++ security/is-request-valid.js
@@ -13,18 +13,28 @@ module.exports = function isRequestValid(context, req, cb) {
   if(uri.query.messageHash && uri.query.dt) {
       var headers = req.headers,
       queryString = uri.query,
-      body = JSON.stringify(req.body),
       requestDate = new Date(queryString.dt),
       currentDate = new Date(),
-      diff = (currentDate - requestDate) / 1000;
-
+      diff = (currentDate - requestDate) / 1000,
+      messageHash = decodeURI(queryString.messageHash);
+       
       if(Object.keys(req.body).length == 0) {
         return cb(new Error(util.format("Unauthorized access from %s -- Missing request body", headers.host)));  
       }
       
-      req.pipe(hashStream(context.sharedSecret, queryString.dt, body)).pipe(concat(function (hash) {
-        if (hash !== queryString.messageHash || diff > timeout) {
-          return cb(new Error(util.format("Unauthorized access from %s, %s, %s Computed: %s", headers.host, queryString.messageHash, queryString.dt, hash)));
+      var bodyKeys = Object.keys(req.body);
+      var bodyString = "";
+      
+      for(var i = 0; i < bodyKeys.length; i++) {
+          bodyString += (bodyKeys[i] + '=' + req.body[bodyKeys[i]]);
+          if(i < (bodyKeys.length - 1)) {
+              bodyString += '&';
+          }
+      }
+          
+      req.pipe(hashStream(context.sharedSecret, queryString.dt, bodyString)).pipe(concat(function (hash) {
+        if (hash !== messageHash || diff > timeout) {
+          return cb(new Error(util.format("Unauthorized access from %s, %s, %s Computed: %s", headers.host, messageHash, queryString.dt, hash)));
         } else {
           return cb(null);
         }
