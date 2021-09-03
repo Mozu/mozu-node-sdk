7c636ca6963cede9cd0ad98f9c4afe84515e9998
thomas phipps <thomas.phipps@kibocommerce.com>
2021-09-02 22:48:09 -0500

    2.15.7

---
+++ phipps copy.js
@@ -0,0 +1,37 @@
+// var json = require('/Users/thomas.phipps/Downloads/fulfiller_swagger.json');
+
+
+// qs = {}
+
+// for (pathKey in json.paths) {
+//     let pathObj = json.paths[pathKey];
+//     for (methKey in pathObj) {
+
+//         pathObj[methKey].parameters.filter(x => x.in == 'query').map(x => x.name).forEach(x => {
+//             qs[x] = `${x}={${x}}`
+//         })
+
+//     }
+
+// }
+
+var f = 0;
+var ds = doStuff()
+ds.then(res => {
+    console.log(res);
+}).catch(err => {
+    console.error(err);
+});
+
+
+
+async function doStuff() {
+    var client = require('./clients/commerce/catalog/admin/product')();
+
+
+    var res = await client.getProducts({
+        
+    });
+    return res;
+}
+
